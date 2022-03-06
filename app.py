bb2de073b8982ad15a3486ee7148da6da68d3421
Jovana Kovacevic <jovana1005@gmail.com>
2022-03-06 16:34:56 +0100

    adding database connection

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+database_path = os.getenv('DATABASE_URL', 'DATABASE_URL_WAS_NOT_SET?!')
+database_path = database_path.replace('postgres://', 'postgresql://')
+app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1',port=5000,debug=True)
+  port = int(os.environ.get("PORT",5000))
+  app.run(host='127.0.0.1',port=port,debug=True)
