d5aafc5a5d2015d3013c6d66f1a69855e7f0c039
Jovana Kovacevic <jovana1005@gmail.com>
2022-03-27 21:51:05 +0200

    added infoWindow

--- static/map.js
+++ static/map.js
@@ -1,21 +1,30 @@
-let map
-let markers
-
 function initMap() {
   const berlin = new google.maps.LatLng(52.51807415366815, 13.403719548295928)
 
-  map = new google.maps.Map(document.getElementById("map"), {
+  const map = new google.maps.Map(document.getElementById("map"), {
     center: berlin,
     zoom: 12,
   })
 
-  markers = venues.map(venue => {
-    const marker = new google.maps.Marker({
-      map: map,
-      position: venue.location
-    });
+  const infoWindow = new google.maps.InfoWindow()
+  venues.forEach(venue => initMarker(venue, map, infoWindow))
+}
 
-    marker.profile = venue
-    return marker
+function initMarker(venue, map, infoWindow) {
+  const marker = new google.maps.Marker({
+    map: map,
+    position: venue.location
+  })
+
+  marker.addListener('click', () => {
+    infoWindow.setContent(
+      `<div class='venue-name'>${venue.name}</div>
+      <div class="venue-address">${venue.address}</div>`)
+
+    infoWindow.open({
+      anchor: marker,
+      map,
+      shouldFocus: false,
+    })
   })
 }
